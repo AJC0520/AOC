b1c364ea3e84a9d788ac2f5cfac52579ae54fad3
Asgeir Jacobsen <asgeija@stud.ntnu.no>
2025-12-05 10:56:57 +0100

    part1

--- ac2025/day5/desember5.py
+++ ac2025/day5/desember5.py
@@ -11,7 +11,15 @@ for r in ranges:
     range_split = r.split("-")
     parsed_ranges.append((int(range_split[0]),int(range_split[1])))
 
+accepted_amount = 0
+i = 0
+for id in ids:
+    for start, end in parsed_ranges:
+        if int(id) >= start and int(id) <= end:
+            accepted_amount +=1
+            break
 
+print(f"Accepted amount : {accepted_amount}")
 
 sorted_ranges = sorted(parsed_ranges)
 merged = []
@@ -22,7 +30,7 @@ for start, end in sorted_ranges:
         merged[-1][1] = max(merged[-1][1], end)
 
 total_ids = sum(end - start + 1 for start, end in merged)
-print(f"{total_ids}")
+print(f"Total fresh IDs after merging: {total_ids}")
             
         
        
